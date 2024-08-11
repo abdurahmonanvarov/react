ac4828d1515532d1412da03590153f8252eb33d9
abdurahmonanvarov <dousolo9@gmail.com>
2024-08-11 17:36:49 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, changeName] = useState('Gulzira')
+  const [event, setEvents] = useState([
+    {title: "Solo va Gulzira", id: 1},
+    {title: "Solo va Anora", id: 2},
+    {title: "Solo va Mubins", id: 3},
+  ])
+
+  const button = () => {
+    changeName('Anora')
+    
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>My love : {name}</h1>
+        <button onClick={button}>Change name</button>
+        {event.map((event) => {
+          return (
+            <div key={event.id}>
+              <h1>{event.title}</h1>
+            </div>
+          )
+        })}
     </div>
   );
 }
